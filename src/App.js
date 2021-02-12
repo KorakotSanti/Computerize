23d44a8a22c82a0708b9948814f5ae3305052a34
Korakot.Santiudommongkol <Korakot.Santiudommongkol@ulta.com>
2021-02-12 12:34:06 -0800

    Created Shop Page
    added shop page
    added collection preview component for all the item preview
    added collection item for every indivdual item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-
 import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -14,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
